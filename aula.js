de04d198d5bb41cc5a169690b4dd09435ff3e275
gilsonlevi <gilsonlevi328@gmail.com>
2023-04-27 13:10:15 -0300

    Criando arrays e objetos

--- aula.js
+++ aula.js
@@ -18,4 +18,5 @@ let estaAprovado = true; //boolean
 let sobrenome ; //Undefined
 let corSelecionado = null; // Redefinir um valor
 
-  
+
+
---
+++ aula2.js
@@ -0,0 +1,28 @@
+//Criando objetos
+
+let pessoa = {
+    nome: 'Gilson Levi',
+    idade: 18 ,
+    estaAprovado: true,
+    sobrenome: 'de Lima Alves',
+    sexo: 'Masculino'
+};
+
+//Arrays
+
+//Criação de um array com 3 posições
+let familia = [25,25, 18];
+
+//Mostrando o indice 2 do array "familia"
+console.log(familia[2]);
+
+//Vendo o tamnho do array
+console.log('O tamanho do array é: ' + familia.length);
+
+let pessoas = [pessoa];
+
+console.log(pessoas[0].nome);
+
+let numero = prompt('Digite um número')
+
+console.log(numero)
