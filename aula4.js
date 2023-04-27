ecc2e113e1e02f9417eb0ec80ab4e585c97be2d0
gilsonlevi <gilsonlevi328@gmail.com>
2023-04-27 15:33:58 -0300

    Operadores aritimeticos

---
+++ aula4.js
@@ -0,0 +1,16 @@
+//Function que não tem retorno
+
+function dizerNome(){
+    console.log("Levi")
+}
+
+dizerNome();
+
+//Function que tem um retorno
+function multiplicarPorDois(valor){
+    return valor * 2;
+}
+
+console.log(multiplicarPorDois(5));
+
+let resultado = multiplicarPorDois(5)
---
+++ aula5.js
@@ -0,0 +1,57 @@
+//Operadores Aritiméticos(matemáticos)
+//Operadores Atribuição
+//Operadores de Comparação
+//Operadores Lógicos
+//Operadores Bitwise
+///////////////////////////////////////////////////////////
+
+//Operadores Aritiméticos(matemáticos)
+//let salario = 100;
+// +, -, *, /, **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5**5);
+
+//let idade = 18;
+//console.log(++idade);
+
+///////////////////////////////////////////////////////////
+
+//Operadores Atribuição
+
+//let valorTeclado = 100;
+//valorTeclado += valorTeclado;
+//console.log(valorTeclado)
+
+//Operadores de Igualdade
+//Igualdade estrita
+// console.log(1 === 1);
+
+// Igualdade solta
+// console.log(1 == '1');
+
+// Operador ternario
+
+// let pontos = 200;
+// let tipo = pontos > 100 ? 'premium' : 'comum'
+// console.log(tipo)
+
+//Operador lógico - &&
+//Retorna TRUE se os dois operandos forem true
+
+let maiorDeIdade = false;
+let possuiCarteiraDeTrabalho = true;
+let podeAplicar = maiorDeIdade && possuiCarteiraDeTrabalho;
+
+console.log(podeAplicar);
+
+//Operador lógico - ||      
+
+maiorDeIdade = false;
+possuiCarteiraDeTrabalho = true;
+podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+
+console.log(podeAplicar);
