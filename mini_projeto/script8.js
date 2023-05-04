038e5fa8b29ff9c3548f86eab5c0900e238e2ff9
gilsonlevi <gilsonlevi328@gmail.com>
2023-05-04 09:55:57 -0300

    Mini projetos

---
+++ mini_projeto/script8.js
@@ -0,0 +1,12 @@
+const array = [70,70,60];
+
+function mediaDoAluno(notas) {
+    const soma = 0;
+    for (let nota of notas) {
+        soma += nota;
+    }
+
+    const media = soma/(notas.lenght);
+
+
+}
