fbcdf5b03c36b927cb978980fd4fbb3d21bb3839
gilsonlevi <gilsonlevi328@gmail.com>
2023-05-19 15:47:33 -0300

    Metodos de string

--- jsbasico/aula10.js
+++ jsbasico/aula10.js
@@ -24,5 +24,19 @@ const men = mensagem.toUpperCase();
 console.log(men);
 
 //Saber se contem uma palavra
+console.log(mensagem.includes("minha"));
 
-console.log(mensagem.includes());
+//Saber se começa com uma letra especifica
+console.log(mensagem.startsWith("ta"));
+
+//Saber se termina com um letra especifica
+console.log(mensagem.endsWith("mensagem"));
+
+//Mudar uma palavra
+console.log(mensagem.replace("minha", "sua"));
+
+//Retirar espaços do final e do começo
+console.log(mensagem.trim());
+
+//Repartição da string
+console.log(mensagem.split(' '));
