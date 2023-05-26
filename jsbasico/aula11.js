7b6dc4c16b3d95b8309f208ff2209840f9fa96c5
gilsonlevi <gilsonlevi328@gmail.com>
2023-05-26 07:38:19 -0300

    Template literal

---
+++ jsbasico/aula11.js
@@ -0,0 +1,25 @@
+//Template Literal
+
+// const mensagem = "Oi isso é a minha\n \"primeira\" mensagem";
+
+// console.log(mensagem);
+
+//Object {}
+// Boolean true,false
+// string '',""
+// Template ``
+
+
+const outra = `Oi isso é minha 'primeira' mensagem `;
+
+console.log(outra);
+
+const nome = 'rafael';
+
+const email = 
+
+`Olá ${nome} ${2+2}
+
+Obrigado sla`;
+
+console.log(email)
